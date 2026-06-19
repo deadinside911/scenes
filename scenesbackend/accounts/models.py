6702fef7b6315a6adf89b2f408787546d5a082ea
agentblack6000 <halan.raghav@gmail.com>
2026-06-19 10:20:55 +0530

    Get rid of Django + NextJS project

--- scenesbackend/accounts/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-# Create your models here.
-class Member(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.EmailField()
-
--- scenesbackend/config/settings/development.py
+++
@@ -1,9 +0,0 @@
-from .base import *
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-q3$5e$b(gq(c=b7p%!!!@1)7mq^u)z61tn7445dju7w_)jek%8"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
